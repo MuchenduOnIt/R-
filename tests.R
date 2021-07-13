900e32d94eb3de4ca8897bee90d7b6a7c78081b0
Steve <muchendumaina66@gmail.com>
2021-07-13 19:39:00 +0300

    I did some normality tests

--- tests.R
+++ tests.R
@@ -22,3 +22,28 @@ while(in>f(y)/g(y))
     runif(1)*M
     y=randg(1)
 }
+
+# Normality tests
+install.packages("astsa")
+data(UnempRate, package = "astsa")
+
+# Check whether there any outliers using the boxplot
+boxplot(UnempRate, main="The U.S Unemployment Rate")
+
+# According to the plot there are values that overlap the upperquatile so there 
+# are outliers
+# Check whether the data is normally distributed using a histogram
+hist(UnempRate, main=)
+lines(density(UnempRate), col="blue", lwd=2)
+
+# Check whether the data is normally distributed using a qqnorm test
+qqnorm(UnempRate)
+qqline(UnempRate) #Check whether the data deviates from the line
+# From my own personal point of view if the data is normally distributed,
+# it should be on the line
+
+# Check whether the data is normally distributed using the shapiro test
+shapiro.test(UnempRate)
+
+# We can also generate random numbers and use them to check for normality
+test=round(rnorm(100, 60, 10))
