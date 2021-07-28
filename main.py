8359656d0a7671ba662a274d8514da6abe1ba861
Steve <muchendumaina66@gmail.com>
2021-07-28 19:35:22 +0300

    Automating life tables

---
+++ main.py
@@ -0,0 +1,18 @@
+def simple_automation():
+    Qx = [0.07230,0.02765,0.00851,0.00636,0.00641,0.00899,0.00814,0.00984,0.01497,0.02184,0.03175,0.04509,0.06498]
+    dx = []
+    lx = []
+    Lx = []
+    n = 1
+    deaths = 0
+    starting_cohort = 100000 
+    for i in Qx:
+        deaths = i * starting_cohort
+        starting_cohort = starting_cohort - deaths
+        dx.append(round(deaths, 1))
+        lx.append(round(starting_cohort, 1))
+        
+    print(dx)
+    print(lx)
+    
+simple_automation()
